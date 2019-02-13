3acd4e388dc1cc677d17b38f241a043eff325cc1
James de Winton <james.dewinton@gmail.com>
2019-02-13 11:33:23 +0000

    replacing numbers if they are greater then 10 with 10

--- lib/sorter.rb
+++ lib/sorter.rb
@@ -1,4 +1,11 @@
 def sorter(array)
-  array
+  new_array = []
+  array.each {|x|
+    if x > 10
+    new_array << 10
+    else
+    new_array << x
+    end }
+    new_array
 
 end
--- spec/sorter_spec.rb
+++ spec/sorter_spec.rb
@@ -2,6 +2,6 @@ require 'sorter'
 
 describe "sorter" do
   it "exists" do
-    expect(sorter([10, 11, 12])).to eq [10, 11, 12]
+    expect(sorter([10, 111, 200, 100, 8])).to eq [10, 10, 10, 10, 8]
   end
 end
