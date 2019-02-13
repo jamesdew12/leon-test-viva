ab18a4e2abffc94cd32d48325b03a33d4d543e02
James de Winton <james.dewinton@gmail.com>
2019-02-13 11:49:42 +0000

    finished

--- lib/sorter.rb
+++ lib/sorter.rb
@@ -1,12 +1,18 @@
 def sorter(array)
   number_array = []
   string_array = []
+  letter_array = []
   array.each {|x|
     if x.is_a? String
+      if x.length == 1
+      letter_array << x
+    else
       string_array << x
+    end
     else
     number_array << x
     end }
-    return string_array, number_array
+
+    return string_array.sort, number_array.sort, letter_array.sort
 
 end
--- spec/sorter_spec.rb
+++ spec/sorter_spec.rb
@@ -2,6 +2,10 @@ require 'sorter'
 
 describe "sorter" do
   it "exists" do
-    expect(sorter([10, 10, "string"])).to eq [["string"], [10, 10]]
+    expect(sorter([10, 10, "string", "x"])).to eq [["string"], [10, 10], ["x"]]
+  end
+  it "orders the numbers and letters and words" do
+    expect(sorter([10, 9, "b", "a", "beach", "zebra"])).to eq [["beach", "zebra"], [9, 10], ["a", "b"]]
+
   end
 end
